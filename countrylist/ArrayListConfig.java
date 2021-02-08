f8973fa467555972b496806f6caeb3862470bd77
Sindhu009 <72006726+Sindhu009@users.noreply.github.com>
2021-02-08 21:56:25 +0530

    Add files via upload

---
+++ countrylist/ArrayListConfig.java
@@ -0,0 +1,16 @@
+package com.xworkz.countrylist;
+
+import java.util.ArrayList;
+
+public class ArrayListConfig  {
+	 private ArrayList<String> countryList;
+
+     public ArrayList<String> getCountryList() {
+           return countryList;
+     }
+
+     public void setCountryList(ArrayList<String> countryList) {
+           this.countryList = countryList;
+     }
+
+}
